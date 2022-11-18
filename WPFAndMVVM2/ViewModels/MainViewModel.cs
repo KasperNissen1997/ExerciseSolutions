94a48a64a89a007d57c88759908e691ce77c3867
KasperNissen1997 <KasperNissen1997@gmail.com>
2022-11-18 23:54:19 +0100

    Finished exercise 27.

---
+++ WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Controls;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private PersonRepository personRepo = new PersonRepository();
+
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+
+        public ObservableCollection<PersonViewModel> PersonsVM { get; set; }
+
+        private PersonViewModel _selectedPerson; 
+        public PersonViewModel SelectedPerson {
+            get { 
+                return _selectedPerson; 
+            } 
+            set { 
+                _selectedPerson = value;
+                OnPropertyChanged("SelectedPerson");
+            } 
+        }
+
+        public MainViewModel () {
+            PersonsVM = new ObservableCollection<PersonViewModel>();
+
+            foreach (Person person in personRepo.GetAll()) {
+                PersonsVM.Add(new PersonViewModel(person));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged (string propertyName) {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+
+            if (propertyChanged != null) {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void AddDefaultPerson () {
+            Person person = personRepo.Add("Specify FirstName", "Specify LastName", 0, "Specify Phone");
+            PersonViewModel personVM = new PersonViewModel(person);
+            
+            PersonsVM.Add(personVM);
+            SelectedPerson = personVM;
+        }
+
+        public void DeleteSelectedPerson () {
+            SelectedPerson.DeletePerson(personRepo);
+            PersonsVM.Remove(SelectedPerson);
+        }
+    }
+}
---
+++ WPFAndMVVM2/Views/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFAndMVVM2.ViewModels;
+
+namespace WPFAndMVVM2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel mainVM;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mainVM = new MainViewModel();
+            DataContext = mainVM;
+        }
+
+        private void AddButton_Click (object sender, RoutedEventArgs e) {
+            mainVM.AddDefaultPerson();
+            PersonsListBox.ScrollIntoView(mainVM.SelectedPerson);
+        }
+
+        private void DeleteButton_Click (object sender, RoutedEventArgs e) {
+            mainVM.DeleteSelectedPerson();
+        }
+
+        private void PersonsListBox_SelectionChanged (object sender, SelectionChangedEventArgs e) {
+            if (PersonsListBox.SelectedItem != null) {
+                mainVM.SelectedPerson = (PersonViewModel) PersonsListBox.SelectedItem;
+                DeleteButton.IsEnabled = true;
+            }
+            else {
+                DeleteButton.IsEnabled = false;
+            }
+        }
+    }
+}
