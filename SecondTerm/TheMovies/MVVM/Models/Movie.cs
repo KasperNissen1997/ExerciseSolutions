c1bb4461d693742386379b66649994acd3dc27c0
Kasper Nissen <KasperNissen1997@gmail.com>
2023-02-12 17:49:56 +0100

    Progress made on the implementation of the Movies case.

---
+++ SecondTerm/TheMovies/MVVM/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace TheMovies.MVVM.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Genres { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Instructor { get; set; }
+        public DateOnly PremiereDate { get; set; }
+
+        public Movie(string title, string genres, TimeSpan duration, string instructor, DateOnly premiereDate)
+        {
+            Title = title;
+            Genres = genres;
+            Duration = duration;
+            Instructor = instructor;
+            PremiereDate = premiereDate;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Title + ";");
+            sb.Append(Genres + ";");
+            sb.Append(Duration + ";");
+            sb.Append(Instructor + ";");
+            sb.Append(PremiereDate);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SecondTerm/TheMovies/MVVM/ViewModels/AddMovieViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.MVVM.ViewModels
+{
+    public class AddMovieViewModel
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int DurationHours { get; set; }
+        public int DurationMinutes { get; set; }
+        public string Instructor { get; set; }
+        public DateTime PremiereDateTime { get; set; }
+
+        public AddMovieViewModel()
+        {
+            PremiereDateTime = DateTime.Now;
+        }
+    }
+}
