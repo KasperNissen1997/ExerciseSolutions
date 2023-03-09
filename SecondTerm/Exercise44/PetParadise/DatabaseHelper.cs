7a00860822a06a74243ccc59912855ec72d4dc99
Kasper Nissen <KasperNissen1997@gmail.com>
2023-03-09 11:11:08 +0100

    Finished the last unit tests.

--- SecondTerm/Exercise44/PetParadise/DatabaseHelper.cs
+++ SecondTerm/Exercise44/PetParadise/DatabaseHelper.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace PetParadise
 {
     public class DatabaseHelper
     {
+        private static readonly string connectionString = "Server=10.56.8.36; Database=DB_2023_36; User Id=STUDENT_36; Password=OPENDB_36";
+
         public static List<string> GetAllPetNamesOwnedByOwner(string firstName, string lastName)
         {
             List<string> result = new List<string>();
 
-            // IMPLEMENT THIS!
-            //  ... using SQL statement on the database
+            using (SqlConnection connection = new(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new("SELECT PETPARADISE_PET.PetName FROM " +
+                    "PETPARADISE_PET INNER JOIN PETPARADISE_OWNER ON PETPARADISE_PET.OwnerId = PETPARADISE_OWNER.OwnerId " +
+                    "WHERE PETPARADISE_OWNER.OwnerFirstName = @OwnerFirstName AND PETPARADISE_OWNER.OwnerLastName = @OwnerLastName;", connection);
+
+                command.Parameters.Add("@OwnerFirstName", SqlDbType.NVarChar).Value = firstName;
+                command.Parameters.Add("@OwnerLastName", SqlDbType.NVarChar).Value = lastName;
+
+                using (SqlDataReader dr = command.ExecuteReader())
+                    while (dr.Read())
+                        result.Add(dr["PetName"].ToString());
+            }
 
             return result;
         }
@@ -19,8 +36,25 @@ namespace PetParadise
         {
             List<Treatment> result = new List<Treatment>();
 
-            // IMPLEMENT THIS!
-            //  ... using SQL statement on the database
+            TreatmentRepo treatRepo = new();
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new("SELECT PETPARADISE_TREATMENT.TreatId FROM PETPARADISE_TREATMENT JOIN PETPARADISE_PET " +
+                    "ON PETPARADISE_TREATMENT.PetId = PETPARADISE_PET.PetId " +
+                    "JOIN PETPARADISE_OWNER " +
+                    "ON PETPARADISE_PET.OwnerId = PETPARADISE_OWNER.OwnerId " +
+                    "WHERE PETPARADISE_OWNER.OwnerFirstName = @OwnerFirstName AND PETPARADISE_OWNER.OwnerLastName = @OwnerLastName;", connection);
+
+                command.Parameters.Add("@OwnerFirstName", SqlDbType.NVarChar).Value = firstName;
+                command.Parameters.Add("@OwnerLastName", SqlDbType.NVarChar).Value = lastName;
+
+                using (SqlDataReader dr = command.ExecuteReader())
+                    while (dr.Read())
+                        result.Add(treatRepo.GetById(int.Parse(dr["TreatId"].ToString())));
+            }
 
             return result;
         }
