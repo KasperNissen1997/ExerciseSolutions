662852f4a9a4fb70846f0d860aa7f97219766494
Kasper Nissen <KasperNissen1997@gmail.com>
2023-09-10 09:47:56 +0200

    Started work on second part of tutorial, up to and including "Create a complex data model".

---
+++ 3rdTerm/Week36/ContosoUniversity2/Models/CourseAssignment.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversity2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity2.Models
+{
+    public class CourseAssignment
+    {
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
