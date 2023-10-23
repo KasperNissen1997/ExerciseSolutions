76b9dffe4766643c05231ba06229bc31e80233f7
Kasper Nissen <KasperNissen1997@gmail.com>
2023-10-23 09:40:02 +0200

    Exercise 1 has been finished.

---
+++ 3rdTerm/Week43/Exercise1/SimpleAPI/Program.cs
@@ -0,0 +1,48 @@
+namespace SimpleAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // CORS fix.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyAllowedOrigins",
+                    policy =>
+                    {
+                        policy.WithOrigins("https://localhost:8081") // Note the port is included
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseCors("MyAllowedOrigins");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
