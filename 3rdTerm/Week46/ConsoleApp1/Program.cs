c4f14c70f597f147d273b43c40c52a3f59189d15
Kasper Nissen <KasperNissen1997@gmail.com>
2023-11-16 11:41:06 +0100

    Added a console project for the technology exercise about password hashing.

---
+++ 3rdTerm/Week46/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new();
+
+            int hash1 = program.GetHashValue("mitKodeord");
+            int hash2 = program.GetHashValue("mitAndetKodeord");
+            int hash21 = program.GetHashValue("mitAndetKodeord");
+            int hash3 = program.GetHashValue("mitTredjeKodeord");
+            int hash4 = program.GetHashValue("mitFjerdeKodeord");
+            int hash5 = program.GetHashValue("mitFemteKodeord");
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int GetHashValue(string password)
+        {
+            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            int hashValue = BitConverter.ToInt32(hashBytes, 0);
+            hashValue %= 100;
+
+            if (hashValue < 0)
+                hashValue *= -1;
+
+            return hashValue;
+        }
+    }
+}
