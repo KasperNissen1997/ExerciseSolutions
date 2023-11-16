270bc1274aaee3fe0d595dfaae3d51f521bd18f0
Kasper Nissen <KasperNissen1997@gmail.com>
2023-11-16 12:21:51 +0100

    Renamed the project to PasswordHashTest, and added formatting to the output.

--- 3rdTerm/Week46/PasswordHashTest/Program.cs
+++ 3rdTerm/Week46/PasswordHashTest/Program.cs
@@ -11,12 +11,16 @@ namespace ConsoleApp1
 
             int hash1 = program.GetHashValue("mitKodeord");
             int hash2 = program.GetHashValue("mitAndetKodeord");
-            int hash21 = program.GetHashValue("mitAndetKodeord");
             int hash3 = program.GetHashValue("mitTredjeKodeord");
             int hash4 = program.GetHashValue("mitFjerdeKodeord");
             int hash5 = program.GetHashValue("mitFemteKodeord");
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Expected hash: 71 | Actual hash: {hash1} | Hashed value: \"mitKodeord\"");
+            Console.WriteLine($"Expected hash: 75 | Actual hash: {hash2} | Hashed value: \"mitAndetKodeord\"");
+            Console.WriteLine($"Expected hash: 47 | Actual hash: {hash3} | Hashed value: \"mitTredjeKodeord\"");
+            Console.WriteLine($"Expected hash: 35 | Actual hash: {hash4} | Hashed value: \"mitFjerdeKodeord\"");
+            Console.WriteLine($"Expected hash: 7  | Actual hash: {hash5}  | Hashed value: \"mitFemteKodeord\"");
+            Console.ReadKey(true);
         }
 
         public int GetHashValue(string password)
