ed81f42b7fb31d1ab28c62ae15f7f43f2d7e7226
Kasper Nissen <KasperNissen1997@gmail.com>
2023-11-25 12:45:43 +0100

    Finished the techonology tasks from tasksheet 11.

--- 3rdTerm/Week46/PasswordHashTest/Program.cs
+++ 3rdTerm/Week46/PasswordHashTest/Program.cs
@@ -5,27 +5,72 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        private const int MAX_HASH_VALUE = 99;
+        private const int HASH_VALUE_LENGTH = 2;
+
+
         static void Main(string[] args)
         {
             Program program = new();
+            program.Run();
 
-            int hash1 = program.GetHashValue("mitKodeord");
-            int hash2 = program.GetHashValue("mitAndetKodeord");
-            int hash3 = program.GetHashValue("mitTredjeKodeord");
-            int hash4 = program.GetHashValue("mitFjerdeKodeord");
-            int hash5 = program.GetHashValue("mitFemteKodeord");
-
-            Console.WriteLine($"Expected hash: 71 | Actual hash: {hash1} | Hashed value: \"mitKodeord\"");
-            Console.WriteLine($"Expected hash: 75 | Actual hash: {hash2} | Hashed value: \"mitAndetKodeord\"");
-            Console.WriteLine($"Expected hash: 47 | Actual hash: {hash3} | Hashed value: \"mitTredjeKodeord\"");
-            Console.WriteLine($"Expected hash: 35 | Actual hash: {hash4} | Hashed value: \"mitFjerdeKodeord\"");
-            Console.WriteLine($"Expected hash: 7  | Actual hash: {hash5}  | Hashed value: \"mitFemteKodeord\"");
             Console.ReadKey(true);
         }
 
-        public int GetHashValue(string password)
+        private void Run()
         {
-            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            bool running = true;
+            Console.WriteLine("Enter a password and get the hashed value.");
+            while (running)
+            {
+                Console.Write("Enter password ('q' to end) : ");
+                string answer = Console.ReadLine();
+                if (answer.Equals("q"))
+                {
+                    running = false;
+                    Console.WriteLine("Bye");
+                }
+                else
+                {
+                    Console.WriteLine("Provided hashing algorithm: " + PadZero(ProvidedGetHashValue(answer), HASH_VALUE_LENGTH));
+                    Console.WriteLine("Personal hashing algorithm: " + PadZero(PersonalGetHashValue(answer), HASH_VALUE_LENGTH));
+                }
+            }
+        }
+
+        public string PadZero(int value, int length)
+        {
+            string result = value.ToString();
+            for (int idx = 0; result.Length < HASH_VALUE_LENGTH; idx++)
+            {
+                result = "0" + result;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// The provided implemtnation of a weak hashing algorithm.
+        /// </summary>
+        /// <param name="password">The password to hash.</param>
+        /// <returns>A hash of the password, in the form of an integer between 0 and 99.</returns>
+        public int ProvidedGetHashValue(string password)
+        {
+            int result = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                result += Convert.ToInt16(password[i] * i);
+            }
+            return result % (MAX_HASH_VALUE + 1);
+        }
+
+        /// <summary>
+        /// My custom implementation of a weak hashing algorithm.
+        /// </summary>
+        /// <param name="secret">The string to hash.</param>
+        /// <returns>A hash of secret, in the form of an integer between 0 and 99.</returns>
+        public int PersonalGetHashValue(string secret)
+        {
+            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(secret));
             int hashValue = BitConverter.ToInt32(hashBytes, 0);
             hashValue %= 100;
 
